1238b4b31fba24a796e415fb83718fd139183f78
Shubham <126750270+codezshubham@users.noreply.github.com>
2024-08-24 00:48:17 +0530

    SpringBoot
    This repository is a comprehensive guide to learning Spring Boot, a powerful framework for building Java-based enterprise applications. It contains tutorials, sample projects, and practical examples to help developers of all levels understand and master Spring Boot.

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach(){
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartStuffConstructor: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    // define our destroy method
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuffConstructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes.";
+    }
+}
